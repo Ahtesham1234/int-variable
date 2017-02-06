66994f1d0bc22146835d7ed623c106cc041f2a28
Ahtesham Ul Haq(@hptouchpad519) <hptouchpad519@gmail.com>
2017-02-06 15:05:31 +0500

    Create int-variables.cpp

---
+++ int-variables.cpp
@@ -0,0 +1,16 @@
+//intvariable.cpp
+//demonstrates interger variables
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int var1;     //define var1
+int var2;     //define var2
+
+var1 = 20;  // assign value to var1
+var2 = var1 + 10;  //assign value to var2
+cout<< "var1+10 is " ;
+cout << var2 << endl;
+return 0;
+}
